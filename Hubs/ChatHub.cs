16398acb1a895def46289a26c4637809e0989265
Alexnader Selishchev <ADOConnection@gmail.com>
2021-01-06 03:49:45 +0300

    commit

---
+++ Hubs/ChatHub.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.SignalR;
+
+namespace WebApp.Hubs
+{
+    public class ChatHub : Hub
+    {
+        public async Task Send(dynamic message)
+        {
+
+        }
+    }
+}
